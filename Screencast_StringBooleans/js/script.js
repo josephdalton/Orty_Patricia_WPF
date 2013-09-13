584f8d61bf9bfdf6fe77f3d30fd2235dc2dabda1
Patricia Orty <animdy@email.com>
2013-09-12 23:13:32 -0400

    Screencast_Strings_Booleans- contains notes on strings and booleans

---
+++ Screencast_StringBooleans/js/script.js
@@ -0,0 +1,29 @@
+// JavaScript
+//Variables of Different Types, Numbers, Booleans and Strings
+
+var age; //declaration
+
+age = 32; //definition : Note numbers can just be typed and don't need a containment such as quote markes.
+
+//storage container = value
+
+var yearOfBirth = 1981; //declaring and defining on the same line
+
+alert (age);
+
+alert (yearOfBirth);
+
+var isStudent = false; //Boolean Variables
+
+var firstName = "Patricia Orty"; //note that words and sentances need to be contained in order to render correctly.
+
+var phrase = 'I don\'t know \nYou\'ll have to speak with the professor'; 
+
+//Usage of escape character \ to allow use of character normally used for containment to read within containment.
+// Note that \n creates a return and their are many other escape combinations to achieve different effects.
+
+alert (isStudent);
+
+alert (firstName);
+
+alert (phrase);
