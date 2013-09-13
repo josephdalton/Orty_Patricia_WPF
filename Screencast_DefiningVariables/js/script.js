753294e9b04577fbbe8841f6b583dd952d273ca9
Patricia Orty <animdy@email.com>
2013-09-12 22:56:51 -0400

    Screencast Defining Variables - Good Notes for later on how to declare and how to define variables

---
+++ Screencast_DefiningVariables/js/script.js
@@ -0,0 +1,13 @@
+// JavaScript
+
+var age; //declaration
+
+age = 32; //definition
+
+//storage container = value
+
+var yearOfBirth = 1981; //declaring and defining on the same line
+
+alert (age);
+
+alert (yearOfBirth)
