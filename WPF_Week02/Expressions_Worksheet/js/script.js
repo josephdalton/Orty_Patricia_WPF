7695826625e25667e27d5c76bfc44a0b63a6e63d
Patricia Orty <animdy@email.com>
2013-09-15 18:56:59 -0400

    Slice of Pie: Part I Problem from WPF - Expressions Worksheet Assignment

--- WPF_Week02/Expressions_Worksheet/js/script.js
+++ WPF_Week02/Expressions_Worksheet/js/script.js
@@ -19,3 +19,16 @@ console.log("Sparky is " + ageOfSparky + " human years old which is " + dogYears
 
 
 //Slice of Pie : Part 1
+// creating an expression to calculate how many slices of pizza party goers will have from X number of pizzas ordered.
+
+var slicesPer = 8;
+var partyGoers = 8;
+var pizzasOrdered = 1;
+
+//multiply Pizzas ordered by the number of slices in a pizza then divide by PartyGoers.
+
+var slicesToGoers = pizzasOrdered * slicesPer / partyGoers;
+
+//printing out the output of slicesToGoers
+
+console.log("Each person ate " + slicesToGoers + " slices of pizza at the party.")
