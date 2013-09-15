e66b75b089998c7b0b02d3b8a1bd64ed7a6de948
Patricia Orty <animdy@email.com>
2013-09-15 18:25:49 -0400

    Screencast Arithmetic Operators

---
+++ WPF_Week02/Activity_Expressions/ScreenCast_ArithmeticOperators/js/script.js
@@ -0,0 +1,10 @@
+// Expressions - Arithmetic Operators
+
+// area of the triangle is half the width times the height
+
+var width = 4;
+var height = 5;
+
+var area = width * height /2;
+
+console.log(area);
