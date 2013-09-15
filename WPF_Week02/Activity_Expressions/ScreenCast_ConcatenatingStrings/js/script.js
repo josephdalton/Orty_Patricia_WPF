e44f2beef7d5186bfbd6225e80876b1fbd4d2621
Patricia Orty <animdy@email.com>
2013-09-15 17:20:40 -0400

    Screencast - ConcatenatingStrings

---
+++ WPF_Week02/Activity_Expressions/ScreenCast_ConcatenatingStrings/js/script.js
@@ -0,0 +1,32 @@
+// Expressions - Concatenating Strings
+
+var firstName = "Patti";
+
+var lastName = "Orty";
+
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+// be carefull not to confuse adding numbers and concatinating strings
+
+var a = "6";
+var b = "7";
+
+var result = a + b;
+
+console.log(result);
+
+// this is going to show result as 67 because the numbers are between " " they are recognized as text not numbers
+// so the a + b is a concatination of those two characters 6 & 7
+// to have them as numbers and to have a + b be addition then the numbers can't be in "
+
+var c = 6;
+var d = 7;
+
+var addition = c + d;
+
+console.log(addition);
+
+
+
