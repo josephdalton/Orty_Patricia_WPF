271f67562541eec0a3d8c4c26f7c3a3ac3150978
Patricia Orty <animdy@email.com>
2013-09-15 18:17:16 -0400

    Screencast Assignment Operators

---
+++ WPF_Week02/Activity_Expressions/ScreenCast_AssignmentOperators/js/script.js
@@ -0,0 +1,28 @@
+// Expressions - Assignment Operators
+
+var a = 3;
+
+a += 4;
+
+// works the same as a = a + 4;
+
+console.log(a)
+
+var b = 3;
+
+b *= 4;
+
+//works the same as b = b * 4; can divide or subtract the same way /= -=
+
+console.log(b);
+
+var c = 3;
+
+c ++;
+
+console.log(c);
+
+// to increase a variable by 1 you can use ++ instead of += 1
+
+// to decrease a variable by 1 you can use -- instead of -=1
+// only works with 1 and they are called increment or decrement operators
