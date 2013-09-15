b8f333de4b0c8c4e2205a0bc70580b38e0e7e3a3
Patricia Orty <animdy@email.com>
2013-09-15 16:13:20 -0400

    Screencast Expressions : has notes on assignment operators

---
+++ WPF_Week02/Activity_Expressions/ScreenCast_Expressions/js/script.js
@@ -0,0 +1,22 @@
+// Expressions
+
+var a = 2; //sets up our variable a and defines it with 2
+a + 3; // add 3 to a 
+
+// output would still show variable 2 as a because the a + 3; didn't change var a .
+//doesn't show a + 3 because it doesn't have an assignment operator... nothing is storing the a + 3
+
+console.log(a);
+
+//you can add in the console output shown below:
+
+console.log(a + 3);
+
+//or you can store it by changing the a variable or making the output a new variable as below
+
+var b = a + 3;
+
+console.log("variable a = " + a + " & " + "variable b = " + b);
+
+//shows that a was not changed and addition was saved in the assignment operator : variable b
+
