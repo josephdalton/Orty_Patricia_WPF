ca06db1704aef40fdbba0b5f8103b34aa5520664
Patricia Orty <animdy@email.com>
2013-09-15 16:28:19 -0400

    ScreenCast - Modulo notes

---
+++ WPF_Week02/Activity_Expressions/ScreenCast_Modulo/js/script.js
@@ -0,0 +1,16 @@
+// Expressions - Modulo Operator
+
+var remainder = 32 / 10;
+
+console.log("The result of 32 / 10 = " + remainder);
+
+var remainder1 = 32 % 10;
+
+console.log("The use of Modulo Operator...typed as 32 % 10 will show you the remainder : " + remainder1);
+
+var remainderEven = 32 % 2;
+
+console.log("The use of Modulo can be used to see if a number is even : as even #s have no remainders ie 32 % 2 = " + remainderEven)
+
+
+
