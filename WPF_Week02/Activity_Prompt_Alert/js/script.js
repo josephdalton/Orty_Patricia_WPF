167430a77a4ddd98e0b30ba38918044ccc772f24
Patricia Orty <animdy@email.com>
2013-09-15 20:29:03 -0400

    Screencast Prompt Alert

---
+++ WPF_Week02/Activity_Prompt_Alert/js/script.js
@@ -0,0 +1,17 @@
+// Screencast : Prompt and Alert
+
+var userInput = prompt("Enter your year of birth:");
+
+console.log(userInput);
+
+//gathering information to calculate area of a rectangle
+
+var width = prompt("We are calculating the area of a rectange. \nPlease enter the width.");
+var height = prompt("Please enter the height.");
+
+var area = width * height; 
+//calculates with that information
+
+//console.log(area); //prints it out in console
+
+alert("The area of your rectangle is " + area + "sq. feet.");
