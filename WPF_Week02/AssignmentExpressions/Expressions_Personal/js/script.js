25deff15ab053b781813e17b334f67b774a08eb0
Patricia Orty <animdy@email.com>
2013-09-16 23:24:03 -0400

    addtion of explanation for prompts.

--- WPF_Week02/AssignmentExpressions/Expressions_Personal/js/script.js
+++ WPF_Week02/AssignmentExpressions/Expressions_Personal/js/script.js
@@ -15,3 +15,7 @@ var nameOfDogFood;
 
 //Alert to explain what information prompts are for.
 alert("HELLO\nFollowing will be some questions asked to determine\nhow much dog food you purchase in a year\nand how many lbs. your dog(s) consumes in a year.");
+
+//Defining variables with prompts to gather information for future calculations and console.log readouts.
+
+
