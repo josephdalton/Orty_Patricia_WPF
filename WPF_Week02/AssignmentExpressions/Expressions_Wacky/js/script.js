35765705a6db55571937bd6324092ad70033fc88
Patricia Orty <animdy@email.com>
2013-09-17 20:47:53 -0400

    Description of Goal for the expression.

--- WPF_Week02/AssignmentExpressions/Expressions_Wacky/js/script.js
+++ WPF_Week02/AssignmentExpressions/Expressions_Wacky/js/script.js
@@ -3,3 +3,9 @@
 // 9/15/13
 
 // Assignment : Expressions - Wacky
+
+//This expression is suppost to be Wacky, soooo I went with an absurd theme of sock Gnomes. 
+//Probably had to do with that I just did laundry but anyway... 
+//this expression's goal is going to be to calculate the number of socks a person would have to pay to the sock Gnomes,
+//in order to have no socks missing after the drying of their clothes for one year. Enjoy!
+
