1d74993b530134b6d9901280c31d6aa3deb90680
Patricia Orty <animdy@email.com>
2013-09-19 21:15:18 -0400

    Screencast - Relational Expression - good notes

---
+++ WPF_Week03/Screencasts_Conditionals/RelationalExpression/js/script.js
@@ -0,0 +1,13 @@
+// Conditional Logic - Relational Expressions
+
+var kidHeight = 49;
+var minHeight = 48;
+
+//if the kid is over 48 inches in height : good to state out if than statemend in plain english prior to coding it.
+//can compair with a static number...ie. kidHeight > 48 or can compair to another variable as shown.
+
+if(kidHeight > minHeight){ 
+	//code performed if condition is true
+	console.log("You can ride the coaster!");
+}
+
