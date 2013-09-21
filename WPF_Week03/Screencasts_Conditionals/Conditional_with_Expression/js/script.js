1fd2ef4af301837381b82b76048011062dab8403
Patricia Orty <animdy@email.com>
2013-09-21 13:43:16 -0400

    Screencast - Conditional with Expression example

---
+++ WPF_Week03/Screencasts_Conditionals/Conditional_with_Expression/js/script.js
@@ -0,0 +1,14 @@
+// Conditional Logic - With and Expression
+
+var kidHeight = 47;
+var minHeight = 48;
+var sneakerLift = 2;
+
+//if the kid is over 48 inches in height : good to state out if than statemend in plain english prior to coding it.
+//can compair with a static number...ie. kidHeight > 48 or can compair to another variable as shown.
+
+if(kidHeight + sneakerLift > minHeight){ 
+	//code performed if condition is true
+	console.log("You can ride the coaster!");
+}
+
