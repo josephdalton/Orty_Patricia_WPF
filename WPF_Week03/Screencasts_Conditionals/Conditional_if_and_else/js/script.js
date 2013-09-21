875bdd7a3331bbb6dc57912bdef485a0f737fc86
Patricia Orty <animdy@email.com>
2013-09-21 13:57:15 -0400

    Screencast - If & Else : example with two if statements

---
+++ WPF_Week03/Screencasts_Conditionals/Conditional_if_and_else/js/script.js
@@ -0,0 +1,20 @@
+// Conditional Logic - if & Else
+
+var kidHeight = 49;
+var minHeight = 48;
+
+//if the kid is over 48 inches in height : good to state out if than statemend in plain english prior to coding it.
+//can compair with a static number...ie. kidHeight > 48 or can compair to another variable as shown.
+
+if(kidHeight > minHeight){ 
+	//code performed if condition is true
+	console.log("You can ride the coaster!");
+}
+
+//can add another if statement to produce a printout if kidHeight is under minHeight.
+//Note that would have to use <= as if I just use < and exact minHeight of 48 would not produce a printout
+//as its not greater and its not less than... but exact... so be careful!
+
+if (kidHeight < minHeight){
+	console.log("Sorry kid, you've got some growing to do first!");
+}
