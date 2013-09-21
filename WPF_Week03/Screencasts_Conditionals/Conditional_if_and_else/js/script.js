15948e6a68e42ecdf8df987c70d0016b9b9c9ec4
Patricia Orty <animdy@email.com>
2013-09-21 14:07:03 -0400

    Screencast - If & Else : example of else statement and notes on why better than two if statements

--- WPF_Week03/Screencasts_Conditionals/Conditional_if_and_else/js/script.js
+++ WPF_Week03/Screencasts_Conditionals/Conditional_if_and_else/js/script.js
@@ -1,6 +1,6 @@
 // Conditional Logic - if & Else
 
-var kidHeight = 49;
+var kidHeight = 46;
 var minHeight = 48;
 
 //if the kid is over 48 inches in height : good to state out if than statemend in plain english prior to coding it.
@@ -9,12 +9,17 @@ var minHeight = 48;
 if(kidHeight > minHeight){ 
 	//code performed if condition is true
 	console.log("You can ride the coaster!");
+}else{
+	//code performed if condition is false
+	console.log("Sorry kid, you've got some growing to do first!");
 }
 
 //can add another if statement to produce a printout if kidHeight is under minHeight.
 //Note that would have to use <= as if I just use < and exact minHeight of 48 would not produce a printout
 //as its not greater and its not less than... but exact... so be careful!
 
-if (kidHeight < minHeight){
-	console.log("Sorry kid, you've got some growing to do first!");
-}
+//if (kidHeight < minHeight){
+//	console.log("Sorry kid, you've got some growing to do first!");
+//}
+//Now for something easier that doesn't require another if statement with a relational opperator.
+//use ELSE...see above.
