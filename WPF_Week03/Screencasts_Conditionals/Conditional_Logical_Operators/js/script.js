07c3669ee9563064f698748db1f44f83e3b26261
Patricia Orty <animdy@email.com>
2013-09-22 15:14:40 -0400

    Screencast: Logical Operators - Using && logical operator

--- WPF_Week03/Screencasts_Conditionals/Conditional_Logical_Operators/js/script.js
+++ WPF_Week03/Screencasts_Conditionals/Conditional_Logical_Operators/js/script.js
@@ -5,17 +5,23 @@ var iPhonePrice = 199.99;
 var paycheck = 200;
 
 //if the price of the phone is less than our budget AND if our paycheck is over 300
-if(iPhonePrice < budget){
+//if(iPhonePrice < budget){
+//	console.log("We can buy the phone!");
+//}else{
+//	console.log("No phone for you!!");
+//}
+
+//if(paycheck > 300){
+//	console.log("We can buy the phone!");
+//}else{
+//		console.log("No phone for you!!");
+//}
+
+//problem with using these two ifs... you get two messages and two conflicting messages.
+//Now the problem is to combine the two conditional statements
+
+if(iPhonePrice < budget && paycheck > 300){
 	console.log("We can buy the phone!");
 }else{
 	console.log("No phone for you!!");
 }
-
-if(paycheck > 300){
-	console.log("We can buy the phone!");
-}else{
-		console.log("No phone for you!!");
-}
-
-//problem with using these two ifs... you get two messages and two conflicting messages.
-//Now the problem is to combine the two conditional statements
