823b6f1ecf713226dbc1a83fb0ad6c0988f06c7a
Patricia Orty <animdy@email.com>
2013-09-22 15:00:46 -0400

    Screencast: Logical Operators - Initial if else statement

---
+++ WPF_Week03/Screencasts_Conditionals/Conditional_Logical_Operators/js/script.js
@@ -0,0 +1,12 @@
+// Conditional Logic - Logical Operators
+
+var budget = 300;
+var iPhonePrice = 199.99;
+var paycheck = 200;
+
+if(iPhonePrice < budget){
+	console.log("We can buy the phone!");
+}else{
+	console.log("No phone for you!!");
+}
+
