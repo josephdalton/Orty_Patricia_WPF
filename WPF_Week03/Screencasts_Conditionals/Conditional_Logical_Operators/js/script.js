8a0d3f9b0c0a0910bedcbd487da9af33e3729c02
Patricia Orty <animdy@email.com>
2013-09-22 18:57:10 -0400

    Screencast: Logical Operators - Example of || (or operator) usage

--- WPF_Week03/Screencasts_Conditionals/Conditional_Logical_Operators/js/script.js
+++ WPF_Week03/Screencasts_Conditionals/Conditional_Logical_Operators/js/script.js
@@ -1,8 +1,8 @@
 // Conditional Logic - Logical Operators
 
-var budget = 300;
+var budget = 100;
 var iPhonePrice = 199.99;
-var paycheck = 200;
+var wonLottery = true;
 
 //if the price of the phone is less than our budget AND if our paycheck is over 300
 //if(iPhonePrice < budget){
@@ -20,8 +20,20 @@ var paycheck = 200;
 //problem with using these two ifs... you get two messages and two conflicting messages.
 //Now the problem is to combine the two conditional statements
 
-if(iPhonePrice < budget && paycheck > 300){
+//if(iPhonePrice < budget && paycheck > 300){
+//	console.log("We can buy the phone!");
+//}else{
+//	console.log("No phone for you!!");
+//}
+
+//Now if we use || (or Operator)
+//if iPhonePrice is less than our budget OR we won the wonLottery
+
+if(iPhonePrice < budget || wonLottery === true){
 	console.log("We can buy the phone!");
 }else{
-	console.log("No phone for you!!");
+	console.log("No Phone for your!!");
 }
+
+// as wonLottery is a boolean and already given a true value
+//alternatie way to write if is : if(iPhonePrice < budget || wonLottery) -- don't need the triple === (Strict Equality Operator)
