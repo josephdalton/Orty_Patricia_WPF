500dd5ad85c280ec38bf86fc2b95282783ec6786
Patricia Orty <animdy@email.com>
2013-09-23 23:08:38 -0400

    Definition of variable nextStation with prompt

--- WPF_Week03/Conditionals_worksheet/Group1_LastChanceforGas/js/script.js
+++ WPF_Week03/Conditionals_worksheet/Group1_LastChanceforGas/js/script.js
@@ -16,5 +16,6 @@ var nextStation;
 
 //definition of variables with prompts to collect data for calculation.
 
+nextStation = prompt("Can you make it to the next gas station\nOR should you stop NOW and refill?\n\nTo begin calculation please provide distance\nin miles to the next gas station...", 200);
 
 
