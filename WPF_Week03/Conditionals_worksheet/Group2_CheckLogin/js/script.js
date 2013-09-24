36bfb1ebe812bdc9b809b1bb8910436b7b0eb6f9
Patricia Orty <animdy@email.com>
2013-09-24 00:19:11 -0400

    Explanation of expression goal

--- WPF_Week03/Conditionals_worksheet/Group2_CheckLogin/js/script.js
+++ WPF_Week03/Conditionals_worksheet/Group2_CheckLogin/js/script.js
@@ -3,3 +3,9 @@
 // 9/23/13
 
 // Conditionals Worksheet - Group 2 : Check the Login
+
+//Expression's goal is to verify if the username and password entered are correct.
+//if both are correct then console.log Welcome, username!
+//if username is incorrect then console.log User not found. Try Again.
+//if password is incorrect then console.log Password does not match our records.
+
