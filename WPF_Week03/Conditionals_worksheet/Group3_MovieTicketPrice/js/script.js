b88982b6ba2fae7e181ced33848c59fbbdb4f9a0
Patricia Orty <animdy@email.com>
2013-09-24 01:09:26 -0400

    Explanation of delcaration of variables

--- WPF_Week03/Conditionals_worksheet/Group3_MovieTicketPrice/js/script.js
+++ WPF_Week03/Conditionals_worksheet/Group3_MovieTicketPrice/js/script.js
@@ -7,4 +7,4 @@
 /*Conditional Expression Goal is to determine if movie goer gets discount priced tickets based on
 if they are 55 and over or under 10 OR watching a movie between 3 and 5 PM*/
 
-
+//Declaration of variables necessary in expression.
