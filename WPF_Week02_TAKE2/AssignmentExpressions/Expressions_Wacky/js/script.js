3b6cb439e81971b2a3b507155d3fe18c21545b03
Patricia Orty <animdy@email.com>
2013-10-10 19:10:32 -0400

    Definition of variable howOld with prompt

--- WPF_Week02_TAKE2/AssignmentExpressions/Expressions_Wacky/js/script.js
+++ WPF_Week02_TAKE2/AssignmentExpressions/Expressions_Wacky/js/script.js
@@ -15,3 +15,4 @@ var howManyPeople;
 alert("Wondering in the woods and see what looks like a falling star? \nUpon further inspection you find it's not a star but a sparkling lil' person with wings? \nWell you've found a fallen fairy and how do you revive this fallen will 'o wisp?\nCLAP! But for how long you ask? Answer the following questions to find out!");
 
 //Defining previously declared variables as prompts to gather information for calculation.
+howOld = prompt("How old are you today?");
