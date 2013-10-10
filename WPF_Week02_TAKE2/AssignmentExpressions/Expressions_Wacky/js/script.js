efae3da42dc793b49650ec33c19b9a906c24c02d
Patricia Orty <animdy@email.com>
2013-10-10 18:58:05 -0400

    Explanation of declaration of variables.

--- WPF_Week02_TAKE2/AssignmentExpressions/Expressions_Wacky/js/script.js
+++ WPF_Week02_TAKE2/AssignmentExpressions/Expressions_Wacky/js/script.js
@@ -5,3 +5,5 @@
 // Assignment : Expressions - Wacky
 
 //Goal of this calculation will be to determine how many claps it will take to revive a fallen fairy, like Tinkerbell in Disney's Peter Pan.
+
+//Declaration of variables used in calculation
