190e42f630eb643d2e08a56252a37c5039eee639
Patricia Orty <animdy@email.com>
2013-10-10 19:09:36 -0400

    Explanation of definition of variables as prompts

--- WPF_Week02_TAKE2/AssignmentExpressions/Expressions_Wacky/js/script.js
+++ WPF_Week02_TAKE2/AssignmentExpressions/Expressions_Wacky/js/script.js
@@ -13,3 +13,5 @@ var howManyPeople;
 
 //Alert message to explain the prompts to collect information for the calculation.
 alert("Wondering in the woods and see what looks like a falling star? \nUpon further inspection you find it's not a star but a sparkling lil' person with wings? \nWell you've found a fallen fairy and how do you revive this fallen will 'o wisp?\nCLAP! But for how long you ask? Answer the following questions to find out!");
+
+//Defining previously declared variables as prompts to gather information for calculation.
