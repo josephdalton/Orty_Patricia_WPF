4b25c278d271fb09206bf22d6f21998f5860e990
Patricia Orty <animdy@email.com>
2013-10-10 19:08:38 -0400

    Setup of Alert to explain future prompts

--- WPF_Week02_TAKE2/AssignmentExpressions/Expressions_Wacky/js/script.js
+++ WPF_Week02_TAKE2/AssignmentExpressions/Expressions_Wacky/js/script.js
@@ -12,3 +12,4 @@ var whatHour;
 var howManyPeople;
 
 //Alert message to explain the prompts to collect information for the calculation.
+alert("Wondering in the woods and see what looks like a falling star? \nUpon further inspection you find it's not a star but a sparkling lil' person with wings? \nWell you've found a fallen fairy and how do you revive this fallen will 'o wisp?\nCLAP! But for how long you ask? Answer the following questions to find out!");
