898afc332460ff19464aa81a0bec0797dafcb315
Patricia Orty <animdy@email.com>
2013-10-10 21:20:08 -0400

    Definition of variable hourlyRate with prompt

--- WPF_Week02_TAKE2/AssignmentExpressions/Expressions_Industry/js/script.js
+++ WPF_Week02_TAKE2/AssignmentExpressions/Expressions_Industry/js/script.js
@@ -16,3 +16,4 @@ alert("Please answer the following questions to determing the profit of your las
 
 //Definition of variables with prompts to gather information for calculation.
 supplyCosts = prompt("What is the total cost of project supplies?");
+hourlyRate = prompt("What is your hourly rate charge?");
