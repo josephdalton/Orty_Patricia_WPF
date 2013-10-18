76481f221da5ad217c0206d1178f665fcbf6d9d2
Patricia Orty <animdy@email.com>
2013-10-17 21:55:02 -0400

    Declaration of variable numberGuests with prompt

--- WPF_Week03_TAKE2/AssignmentConditionals/Conditionals_Industry/js/script.js
+++ WPF_Week03_TAKE2/AssignmentConditionals/Conditionals_Industry/js/script.js
@@ -14,3 +14,4 @@ var childGuests;
 alert("Your company put you in charge of ordering food for the holiday dinner.\nThey decided to do a traditional Turkey dinner and you need to figure out how many lbs. of Turkey to purchase.\nAnswer the following questions to ensure that all your co-workers and their guests have enough Turkey to eat.");
 
 //Declaration of variables as prompts to gather information for calculation.
+numberGuests = prompt("How many guests will be in attendance?");
