59295238e51ebf9b496caaac986a6e924476ec9d
Patricia Orty <animdy@email.com>
2013-10-17 23:15:29 -0400

    Addition of alert

--- WPF_Week03_TAKE2/AssignmentConditionals/Conditionals_Wacky/js/script.js
+++ WPF_Week03_TAKE2/AssignmentConditionals/Conditionals_Wacky/js/script.js
@@ -12,3 +12,5 @@ var hrsAlone;
 var scoldedHrsAgo;
 
 //Explanation of following prompts with alert
+alert("Our furry friends can get very creative with how they occupy themselves when we are away.\nThe following questions are to determine if your puppy is going to leave your shoes alone while you are out and about.");
+
