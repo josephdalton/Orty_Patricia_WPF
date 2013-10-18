d42c431ff633105807c2357240444c0b7c574b02
Patricia Orty <animdy@email.com>
2013-10-17 20:01:44 -0400

    Goal stated for Conditionals Personal Assignment

--- WPF_Week03_TAKE2/AssignmentConditionals/Conditionals_Personal/js/script.js
+++ WPF_Week03_TAKE2/AssignmentConditionals/Conditionals_Personal/js/script.js
@@ -3,3 +3,8 @@
 // 10/17/13
 
 // Assignment : Conditionals - Personal
+
+/* Goal: To determine if a learner-permit driver can setup their driving test by seeing if 
+the driver is old enough & either has enough hours of driving with a licensed driver (72 hrs.) 
+or had gotten a C or better in Drivers Ed */
+
