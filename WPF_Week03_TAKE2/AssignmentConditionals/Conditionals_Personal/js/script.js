70b56d41fc5212f84fe0970b5320768bbdd9a2c0
Patricia Orty <animdy@email.com>
2013-10-17 21:26:00 -0400

    Fix of ternary with changege of data request for gradeDriversEd from LETTER grade to NUMBER grade

--- WPF_Week03_TAKE2/AssignmentConditionals/Conditionals_Personal/js/script.js
+++ WPF_Week03_TAKE2/AssignmentConditionals/Conditionals_Personal/js/script.js
@@ -6,7 +6,7 @@
 
 /* Goal: To determine if a learner-permit driver can setup their driving test by seeing if 
 the driver is old enough & either has enough hours of driving with a licensed driver (72 hrs.) 
-or had gotten a C or better in Drivers Ed */
+or had gotten a 70 or better in Drivers Ed */
 
 //Declaration of variables used to gain information for calculation.
 var ageDriver;
@@ -19,7 +19,7 @@ alert("Want to find out if you can schedule your road test?\nThen answer the nex
 //Defining variables as prompts to gain input for calculation
 ageDriver = prompt("How old are you?");
 hrsDriven = prompt("What is the total number of hours you have driven with a licensed driver in your vehicle?");
-gradeDriversED = prompt("What LETTER grade did you get in Driver's Ed class?");
+gradeDriversED = prompt("What NUMBER grade did you get in Driver's Ed class?");
 
 //Addition of ParseInt to ageDriver and gradeDriversED so that input data is treated as a whole number.
 ageDriver = parseInt(ageDriver);
@@ -30,7 +30,7 @@ gradeDriversED = parseInt(gradeDriversED);
 hrsDriven = parseFloat(hrsDriven);
 
 //Usage of Ternary to determine if driver can or can not schedule a road test.
-var overallResult = (ageDriver >= 17 && (hrsDriven >= 72 || gradeDriversED <= C)) ? "You meet the criteria to schedule your road test. GOOD LUCK!" : "You do not meet the critera to schedule your road test. Reasons are listed below: ";
+var overallResult = (ageDriver >= 17 && (hrsDriven >= 72 || gradeDriversED >= 70)) ? "You meet the criteria to schedule your road test. GOOD LUCK!" : "You do not meet the critera to schedule your road test. Reasons are listed below: ";
 
 //Console.log to display overallResult variable information
 console.log(overallResult);
