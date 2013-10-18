0b1fb01188aacfab91c22d9d43c19dbfdf11e38c
Patricia Orty <animdy@email.com>
2013-10-17 21:58:38 -0400

    Explanation of parseInt addition to variables

--- WPF_Week03_TAKE2/AssignmentConditionals/Conditionals_Industry/js/script.js
+++ WPF_Week03_TAKE2/AssignmentConditionals/Conditionals_Industry/js/script.js
@@ -16,3 +16,5 @@ alert("Your company put you in charge of ordering food for the holiday dinner.\n
 //Declaration of variables as prompts to gather information for calculation.
 numberGuests = prompt("How many guests will be in attendance?");
 childGuests = prompt("How many of the attendees will be children under the age of 6?");
+
+//addition of parseInt to varables as half people can't exist and they aren't needed for calculation. Will also make sure information is treated as a number.
